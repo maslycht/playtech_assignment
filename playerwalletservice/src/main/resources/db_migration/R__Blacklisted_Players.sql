2d3347793358ec12193527c3e009bb0e220cf07d
Taras Maslych <taras.maslych@nutiparkla.com>
2020-09-14 01:15:41 +0300

    Added migration file to populate the user blacklist.

---
+++ playerwalletservice/src/main/resources/db_migration/R__Blacklisted_Players.sql
@@ -0,0 +1,2 @@
+insert into PLAYER_BLACKLIST (USERNAME) values ('vir4038o0') on conflict (USERNAME) do nothing;
+insert into PLAYER_BLACKLIST (USERNAME) values ('Drusereebarma2d') on conflict (USERNAME) do nothing;
